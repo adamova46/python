49b12c28c74ea52745ab65b963d655aff6d538f7
adamova46 <adamovaaleksandra46@gmail.com>
2025-11-17 12:09:23 +0300

    Create 1file.py
    практическая номер1

---
+++ 1file.py
@@ -0,0 +1,60 @@
+
+first_num = 9
+second_num = 7.8
+my_str = "start"
+print(first_num, second_num, my_str)
+first_num =5.2
+print(first_num,type(first_num))
+third_num=first_num + second_num
+print(third_num,type(third_num))
+first_num+=5
+second_num+=first_num
+print(first_num, second_num)
+second_num=int(second_num)
+print(first_num, second_num)
+my_str="start&stop"
+print(my_str)
+my_str*=5
+print(my_str)
+
+
+
+path="C:\\Users\\MainAdmin\\Desktop\\programs"
+print(path)
+path+="\\game.exe"
+print(path)
+path="C:\\Users\\MainAdmin\\Desktop\\files"
+path+="\\picture.png"
+print(path)
+path="C:\\Games\\city simulator"
+path*=2
+print("Error path:", path)
+
+
+num_1=7
+num_2=10
+num_3=4
+summ=num_1+num_2+num_3
+print("сумма всех целых чисел",summ)
+num_1=7.9
+num_2=21.3
+num_3=float(num_3)
+summ=num_1+num_2+num_3
+print("сумма всех целых чисел",summ)
+num_1=130
+num_2=4
+num_3=2
+multiplaing=num_1*num_2*num_3
+print("умножение всех чисел",multiplaing)
+division=num_1/num_2/num_3
+print("Деление", division)
+num_1=2
+num_2=3
+num_3=4
+degree=num_1**num_2**num_3
+print("Число в степени",degree)
+num_1=2
+num_2=8
+num_3=5
+match=((43+num_1)+(num_2+67)-(num_3*2))//2
+print(match)
